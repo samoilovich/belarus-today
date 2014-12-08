8e30b2f5d35e6da6c30b98cd04be654543d97ef2
samoilovich <natalia.samoilovich@gmail.com>
2014-12-09 01:50:44 +0300

    fix gradle properties after android studio has been updated

---
+++ app/src/androidTest/java/com/samoilovich/belarustoday/ApplicationTest.java
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+package com.samoilovich.belarustoday;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+=======
+package com.samoilovich.belarustoday;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+>>>>>>> 8be0222428a13e4467051cdcfb9b2f63a534965f
+}
